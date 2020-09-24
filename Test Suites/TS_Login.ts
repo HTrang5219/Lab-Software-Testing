b9b9600052cac2000af21ea51d221eb8aecbdc1e
HTrang5219 <tlht.5219@gmail.com>
2020-09-24 22:36:38 +0700

    Push project

---
+++ Test Suites/TS_Login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a7cecfa3-eaaf-41bb-9eaa-621daf512a1d</testSuiteGuid>
+   <testCaseLink>
+      <guid>8abd8acc-40c1-4366-afd5-bd848a530d86</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4ac2b588-5a7f-4a5c-baed-2b3fa3c3f9be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72d4b815-d221-4772-9dfa-29d9ce687987</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33612c76-c19a-4eba-b1ff-798a9325263c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ddf6729a-53f9-4c48-a1f7-b995e38bc1d9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
